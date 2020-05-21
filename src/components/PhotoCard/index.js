d4ddf11361d7dc3e0506436f4ba266fe4bc8680b
Nahuel Alvarez <nahuel.alvarez@smfconsulting.es>
2020-05-20 21:37:01 -0300

    Custom Hooks: useNearScreen

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,40 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
-
 import { AiOutlineLike, AiTwotoneLike } from 'react-icons/ai'
-
 import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const element = useRef(null)
-
+  const [show, element] = useNearScreen()
   const key = `like-${id}`
-
-  const [show, setShow] = useState(false)
-
   const [liked, setLiked] = useLocalStorage(key, false)
-
-  console.log({ liked })
-
-  useEffect(function () {
-    Promise.resolve(
-      typeof window.IntersectionObserver !== 'undefined'
-        ? window.IntersectionObserver
-        : import('intersection-observer')
-    ).then(() => {
-      const observer = new window.IntersectionObserver(function (entries) {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-      observer.observe(element.current)
-    })
-  }, [element])
-
   const Icon = liked ? AiTwotoneLike : AiOutlineLike
 
   return (
