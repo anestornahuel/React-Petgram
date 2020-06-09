e28c1e1e620e833eac9f1c5476e134a4bd71adff
Nahuel Alvarez <nahuel.alvarez@smfconsulting.es>
2020-06-09 12:17:56 -0300

    Extract ListOfPhotoCards component container

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
-import { withPhotos } from '../../hoc/withPhotos'
 
 // props.data.photos
-const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
+export const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
   return (
     <ul>
       {photos.map(photo => <PhotoCard key={photo.id} id={photo.id} {...photo} />)}
     </ul>
   )
 }
-
-export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
