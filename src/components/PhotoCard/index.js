1eb1d36961f4cdfa9d6ceba58d7b54526134f020
Nahuel Alvarez <nahuel.alvarez@smfconsulting.es>
2020-07-20 16:05:06 -0300

    Like only with registered user

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Article, ImgWrapper, Img } from './styles'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { LikeButton } from '../LikeButton'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
@@ -8,11 +7,8 @@ import { Link } from '@reach/router'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
-  const key = `like-${id}`
-
-  const [liked, setLiked] = useLocalStorage(key, false)
 
   return (
     <Article ref={element}>
@@ -28,12 +24,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               {
                 (toggleLike) => {
                   const handleLikeClick = () => {
-                    !liked && toggleLike({
+                    toggleLike({
                       variables: {
                         input: { id }
                       }
                     })
-                    setLiked(!liked)
                   }
                   return <LikeButton liked={liked} likes={likes} onClick={handleLikeClick} />
                 }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,26 @@ import { ApolloProvider } from 'react-apollo'
 import { App } from './App'
 import Context from './Context'
 
+// The property request will be called before any request to the server
 const client = new ApolloClient({
-  uri: 'https://petgram-server.anestornahuel.now.sh/graphql'
-
+  uri: 'https://petgram-server.anestornahuel.now.sh/graphql',
+  request: operation => {
+    const token = window.sessionStorage.getItem('token')
+    const authorization = token ? `Bearer ${token}` : ''
+    operation.setContext({
+      headers: {
+        authorization
+      }
+    })
+  },
+  onError: error => {
+    // Handle error for expired token
+    const { networkError } = error
+    if (networkError && networkError.result.code === 'invalid_token') {
+      window.sessionStorage.removeItem('token')
+      window.location.href = '/'
+    }
+  }
 })
 
 ReactDOM.render(
