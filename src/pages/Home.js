7c5cf8bbd8e3050e420d76c58e1057660be0bcda
Nahuel Alvarez <nahuel.alvarez@smfconsulting.es>
2020-09-06 22:44:28 -0300

    Add Layout and titles with React Helmet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
+import { Layout } from '../components/Layout'
 
 export const Home = ({ id }) => (
-  <>
+  <Layout title='The pet photo app' subtitle='In petgram you can share and search pet photos'>
     <ListOfCategories />
     <ListOfPhotoCards categoryId={id} />
-  </>
+  </Layout>
 )
